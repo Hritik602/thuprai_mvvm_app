ae3e6c172b5ac476ad4d4eeaf967e63bccbe5036
hritik602 <hritikrajkarn@gmail.com>
2023-08-23 17:54:33 +0545

    feat: add service and request model of sign up.

--- lib/screens/auth/sign_up/service/auth_sign_up_service.dart
+++ lib/screens/auth/sign_up/service/auth_sign_up_service.dart
@@ -1,1 +1,18 @@
-class AuthSignUpService {}
+import 'package:thuprai_mvvm_test/network/api_error.dart';
+import 'package:thuprai_mvvm_test/network/network_common.dart';
+import 'package:thuprai_mvvm_test/screens/auth/sign_up/model/sign_in_response.dart';
+import 'package:thuprai_mvvm_test/screens/auth/sign_up/model/signup_request.dart';
+
+class AuthSignUpService {
+  static const String _signupEnpoint = "/api/signup/";
+
+  Future<SignInResponse> signUpUser(SignUpRequest signUpRequest) async {
+    try {
+      var res = await NetworkCommon.dio
+          .post(_signupEnpoint, data: signUpRequest.toMap());
+      return SignInResponse.fromJson(res.data);
+    } catch (e) {
+      return SignInResponse.withDioException(e as DioExceptions);
+    }
+  }
+}
