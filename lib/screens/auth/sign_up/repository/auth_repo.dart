828f233af4c44a79813c2a9f2f23b175ed603c35
hritik602 <hritikrajkarn@gmail.com>
2023-08-23 18:11:39 +0545

    feat: add auth repository and auth view model.

--- lib/screens/auth/sign_up/repository/auth_repo.dart
+++ lib/screens/auth/sign_up/repository/auth_repo.dart
@@ -1,1 +1,15 @@
-abstract class AuthRepository {}
+import 'package:thuprai_mvvm_test/screens/auth/sign_up/model/sign_in_response.dart';
+import 'package:thuprai_mvvm_test/screens/auth/sign_up/model/signup_request.dart';
+import 'package:thuprai_mvvm_test/screens/auth/sign_up/service/auth_sign_up_service.dart';
+
+abstract class IAuthRepository {
+  Future<SignInResponse> createUser(SignUpRequest signUpRequest);
+}
+
+class AuthRepository extends IAuthRepository {
+  final AuthSignUpService _authSignUpService = AuthSignUpService();
+  @override
+  Future<SignInResponse> createUser(SignUpRequest signUpRequest) async {
+    return await _authSignUpService.signUpUser(signUpRequest);
+  }
+}
---
+++ lib/screens/auth/sign_up/view_model/auth_view_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:thuprai_mvvm_test/screens/auth/sign_up/model/sign_in_response.dart';
+import 'package:thuprai_mvvm_test/screens/auth/sign_up/model/signup_request.dart';
+import 'package:thuprai_mvvm_test/screens/auth/sign_up/repository/auth_repo.dart';
+
+class AuthViewModel extends ChangeNotifier {
+  final AuthRepository _authRepository = AuthRepository();
+
+  ///Create user / Sign up user.
+  Future<SignInResponse> signUpUser(SignUpRequest signUpRequest) async {
+    return await _authRepository.createUser(signUpRequest);
+  }
+}
