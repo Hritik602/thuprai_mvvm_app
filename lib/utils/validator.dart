150804204a4446766748ba3b2024ecacbb05fd83
hritik602 <hritikrajkarn@gmail.com>
2023-08-24 16:14:35 +0545

    fix:make method nullable.

--- lib/utils/validator.dart
+++ lib/utils/validator.dart
@@ -1,27 +1,27 @@
 class Validator {
-  static String validateName(String name) {
+  static String? validateName(String name) {
     if (name.isEmpty) {
       return 'Name is required';
     }
-    return "";
+    return null;
   }
 
-  static String validateEmail(String value) {
+  static String? validateEmail(String value) {
     if (value.isEmpty) {
       return 'Email is required';
     }
     if (!RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value)) {
       return 'Please enter a valid email address';
     }
-    return "";
+    return null;
   }
 
-  static String validatePassword(String password) {
+  static String? validatePassword(String password) {
     if (password.isEmpty) {
       return "Password is required";
     } else if (password.length < 8) {
       return "Password length must be grater than 8 character";
     }
-    return "";
+    return null;
   }
 }
