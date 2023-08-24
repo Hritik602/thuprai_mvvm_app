f567720d88b9bf174d595b4f393c20e954826dc3
hritik602 <hritikrajkarn@gmail.com>
2023-08-24 15:46:24 +0545

    feat: add  common input text field for name,email and password.

--- lib/screens/auth/sign_up/view/sign_up.dart
+++ lib/screens/auth/sign_up/view/sign_up.dart
@@ -1,30 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+import 'package:thuprai_mvvm_test/screens/auth/sign_up/model/sign_in_response.dart';
+import 'package:thuprai_mvvm_test/screens/auth/sign_up/model/signup_request.dart';
+import 'package:thuprai_mvvm_test/screens/auth/sign_up/view_model/auth_view_model.dart';
+import 'package:thuprai_mvvm_test/utils/app_alert_dialog.dart';
+import 'package:thuprai_mvvm_test/utils/app_constant_text.dart';
+import 'package:thuprai_mvvm_test/utils/app_routes.dart';
+import 'package:thuprai_mvvm_test/utils/validator.dart';
+import 'package:thuprai_mvvm_test/widget/input_textfield.dart';
+import 'package:thuprai_mvvm_test/widget/password_text_form_field.dart';
+import 'package:thuprai_mvvm_test/widget/primary_button.dart';
 
-class SignUp extends StatelessWidget {
+class SignUp extends StatefulWidget {
   const SignUp({super.key});
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  late TextEditingController _nameController;
+  late TextEditingController _emailController;
+  late TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController = TextEditingController();
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: SafeArea(
         minimum: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("Sign in "),
-            const TextField(
-              decoration: InputDecoration(border: OutlineInputBorder()),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const TextField(
-              decoration: InputDecoration(border: OutlineInputBorder()),
-            ),
-            ElevatedButton(onPressed: () {}, child: const Text("Click"))
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Sign Up",
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyLarge!
+                    .copyWith(fontSize: 20.sp),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              InputTextfield(
+                controller: _nameController,
+                validator: (value) => Validator.validateName(value!),
+                hintText: ConstantText.nameHintText,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              InputTextfield(
+                  controller: _emailController,
+                  validator: (value) => Validator.validateEmail(value!),
+                  hintText: ConstantText.emailHintText),
+              const SizedBox(
+                height: 20,
+              ),
+              PasswordTextField(
+                controller: _passwordController,
+                hintText: ConstantText.passwordHintText,
+                validator: (value) => Validator.validatePassword(value!),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
         ),
       ),
+      bottomNavigationBar: PrimaryButton(
+        buttonText: "Sign Up",
+        onTap: () {
+          _onClickSignUp();
+        },
+      ),
     );
+  }
+
+  void _onClickSignUp() async {
+    if (_formKey.currentState!.validate()) {
+      showLoadingIndicator(context);
+      SignUpRequest signUpRequest = SignUpRequest(
+          name: _nameController.text.trim(),
+          email: _emailController.text.trim(),
+          password: _passwordController.text.trim());
+      SignInResponse _signResponse =
+          await context.read<AuthViewModel>().signUpUser(signUpRequest);
+      dismissLoadingIndicator();
+      if (_signResponse.dioExceptions == null) {
+        if (!mounted) return;
+        Navigator.pushReplacementNamed(context, AppRoute.homeRoute);
+      } else {
+        dismissLoadingIndicator();
+        if (!mounted) return;
+        showSuccessNAlertDialog(context);
+        return;
+      }
+    }
   }
 }
---
+++ lib/widget/input_textfield.dart
@@ -0,0 +1,44 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class InputTextfield extends StatelessWidget {
+  const InputTextfield({
+    Key? key,
+    required this.controller,
+    this.onChanged,
+    this.onFieldSubmitted,
+    required this.hintText,
+    required this.validator,
+    this.textInputType,
+  }) : super(key: key);
+  final TextEditingController? controller;
+  final Function(String)? onChanged;
+  final Function(String)? onFieldSubmitted;
+  final String? hintText;
+  final String Function(String?) validator;
+  final TextInputType? textInputType;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 0.06.sh,
+      child: TextFormField(
+          keyboardType: textInputType ?? TextInputType.text,
+          controller: controller,
+          validator: validator,
+          decoration: InputDecoration(
+              hintText: hintText ?? "",
+              hintStyle: Theme.of(context).textTheme.labelSmall,
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+              errorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: const BorderSide(color: Colors.red)),
+              focusedBorder:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+              enabledBorder:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(20)))),
+    );
+  }
+}
