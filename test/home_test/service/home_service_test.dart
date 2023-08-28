dea20024a549bb73632b5c0ca2da4a0879d2e046
hritik602 <hritikrajkarn@gmail.com>
2023-08-28 10:22:21 +0545

    feat: add test case for home service include fetch api call all book release list.

---
+++ test/home_test/service/home_service_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:thuprai_mvvm_test/screens/home/model/new_book_release_model.dart';
+import 'package:thuprai_mvvm_test/screens/home/service/home_service.dart';
+
+import 'home_service_test.mocks.dart';
+
+@GenerateMocks([HomeService])
+void main() {
+  MockHomeService mockHomeService = MockHomeService();
+  test('Test for fetching all new book release  ', () async {
+    ///[Arrange]
+    when(mockHomeService.fetchAllNewReleaseBooks())
+        .thenAnswer((realInvocation) => Future.value(NewBookReleaseModel()));
+
+    final result = mockHomeService.fetchAllNewReleaseBooks();
+
+    ///[Act]
+    expect(result, isInstanceOf<Future<NewBookReleaseModel>>());
+  });
+}
