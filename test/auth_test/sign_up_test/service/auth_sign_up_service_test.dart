68452507eede7e009595a8ec0daf5f8bcb25cbe9
hritik602 <hritikrajkarn@gmail.com>
2023-08-29 16:13:19 +0545

    fix: remove unused imports

--- test/auth_test/sign_up_test/service/auth_sign_up_service_test.dart
+++ test/auth_test/sign_up_test/service/auth_sign_up_service_test.dart
@@ -1,9 +1,6 @@
-import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:thuprai_mvvm_test/network/api_error.dart';
-import 'package:thuprai_mvvm_test/network/network_common.dart';
 import 'package:thuprai_mvvm_test/screens/auth/sign_up/model/sign_in_request.dart';
 import 'package:thuprai_mvvm_test/screens/auth/sign_up/model/sign_in_response.dart';
 import 'package:thuprai_mvvm_test/screens/auth/sign_up/model/signup_request.dart';
