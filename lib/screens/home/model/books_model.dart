9272804a5c74612184ff1aa67452e67bfc741c87
hritik602 <hritikrajkarn@gmail.com>
2023-08-29 17:57:06 +0545

    feat: add books model use in view

---
+++ lib/screens/home/model/books_model.dart
@@ -0,0 +1,17 @@
+class BooksModel {
+  String? bookTitle;
+  String? slug;
+  String? frontCover;
+  int? page;
+  int? pages;
+  int? totalCount;
+
+  BooksModel({
+    this.bookTitle,
+    this.slug,
+    this.frontCover,
+    this.page,
+    this.pages,
+    this.totalCount,
+  });
+}
